b834a66ef836d18db8fe86631dec2b40b029d0dc
Sylvia Zarnescu <56455042+sylviazarnescu@users.noreply.github.com>
2022-05-06 21:07:09 -0700

    final project

---
+++ Final.R
@@ -0,0 +1,15 @@
+---
+  title: "ISTA 320 Data Visualization Challenge 7"
+author: "Sylvia Zarnescu"
+date: "Spring 2022"
+output: html_document
+---
+  
+  ```{r setup, include=FALSE}
+# remember to add any other libraries you need here:
+
+library(knitr)
+opts_chunk$set(echo = TRUE)
+library(tidyverse)
+library(RColorBrewer)
+library(ggthemes)
